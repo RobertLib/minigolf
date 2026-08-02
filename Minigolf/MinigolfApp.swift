81d152bdbd5bd3c2303ad4f60e92ec93c0c0d5a0
RobertLib <rob@hotmail.cz>
2026-08-02 07:05:40 +0200

    Initial Commit

---
+++ Minigolf/MinigolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinigolfApp.swift
+//  Minigolf
+//
+//  Created by Robert Libšanský on 02.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct MinigolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
